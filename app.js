e97e8ad128230fb33ced4158d43f8a5775aea4ca
Jan Dubniak <narusteras@gmail.com>
2017-12-08 02:07:52 +0100

    app.js user argv parsing api getting response from user basic input controll

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            alias: 'address',
+            demand: true,
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+// console.log(encodeURIComponent(argv.a));
+
+var encodedAddress = encodeURIComponent(argv.a)
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301+lombard+street+philadelphia',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodedAddress,
     json: true
 }, (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address} \nLattitude ${body.results[0].geometry.location.lat} \nLongtitude ${body.results[0].geometry.location.lng}`);
+
+    if(error) console.log('Unable to connect to Google servers');
+    else if(body.status === 'ZERO_RESULTS') console.log('Unable to find that address');
+    else if(response)
+    if (body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}
+                    Lattitude ${body.results[0].geometry.location.lat} 
+                    Longtitude ${body.results[0].geometry.location.lng}`);
+    }
 });
