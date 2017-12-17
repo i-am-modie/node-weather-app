15a79faf448ae67946f41e73a331091131bc4dce
Jan Dubniak <narusteras@gmail.com>
2017-12-17 21:04:03 +0100

    added axios module changed callbacks to promises using axios module!

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const yargs = require('yargs');
+const axios = require('axios');
 
-const geocode = require('./geocode/geocode');
-const weather = require('./weather/weather');
 
 const argv = yargs
     .options({
@@ -16,23 +15,27 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-// console.log(encodeURIComponent(argv.a));
+var encodedAddress = encodeURIComponent(argv.address);
+var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
 
-geocode.googleAPI(argv.a, (errorMessage, results) => {
-    if(errorMessage){
-        console.log(errorMessage);
+axios.get(geocodeUrl).then( (response) => {
+    if( response.data.status === 'ZERO_RESULTS'){
+        throw new Error('Unable to find that address');
     }
-    else{
-        console.log(`Address: ${results.address}`);
-        weather.getWeather(results.lattitude, results.longtitude, (errorMessage, results) => {
-            if(errorMessage){
-                console.log(errorMessage);
-            }
-            else{
-                console.log(`Pogoda: ${results.summary}`);
-                console.log(`Current temperature: ${results.currentTemp}`);
-                console.log(`Apparent temperature: ${results.apparentTemperature}`);
-            }
-        });
-    }
-});
+    var lattitude = response.data.results[0].geometry.location.lat;
+    var longtitude = response.data.results[0].geometry.location.lng;
+    console.log(`Address: ${response.data.results[0].formatted_address}`);
+    var WeatherURL = `https://api.darksky.net/forecast/943ad4ba86d8fba59f7f3acf63874671/${lattitude},${longtitude}?lang=pl`;
+    return axios.get(WeatherURL);})
+    .then( ( response) => {
+        console.log(`Pogoda: ${response.data.currently.summary}`);
+        console.log(`Current temperature: ${response.data.currently.temperature}`);
+        console.log(`Apparent temperature: ${response.data.currently.apparentTemperature}`);
+    })
+    .catch( (e) => {
+        if(e.code === 'ENOTFOUND'){
+            console.log('Unable to connect to Google API');
+        } else {
+            console.log(e.message);
+        }
+    });
